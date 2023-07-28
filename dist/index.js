e66be9a6e9c53bb84304d3287190476c39fd5b7c
adityagithubraj <adityaraj9843@gmail.com>
2023-07-28 08:06:32 +0530

    ggg

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// console.log("hiii")
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -23,5 +22,5 @@ app.get("/", (req, res) => {
 // Add the routes
 app.use("/", route_1.router);
 // Start the server
-const PORT = Number(process.env.PORT) || 6010;
+const PORT = Number(process.env.PORT) || 3000;
 const server = app.listen(PORT, () => console.log(`runig on port  ${PORT}`));
